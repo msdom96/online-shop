538e7c7465840360b58327714ca28a45843160d4
Juan <sagdom96@gmail.com>
2022-06-14 15:22:28 +0200

    update

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
-const productsController = require('./controllers/products');
-
 app.set('view engine', 'ejs');
-app.set('views', 'views');  
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(productsController.get404);
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,20 +2,22 @@ const path = require('path');
 
 const express = require('express');
 
-const shopControllers = require('../controllers/shop');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', shopControllers.getIndex);
+router.get('/', shopController.getIndex);
 
-router.get('/products', shopControllers.getProducts);
+router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId', shopControllers.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopControllers.getCart);
+router.get('/cart', shopController.getCart);
 
-router.get('/orders', shopControllers.getOrders);
+router.post('/cart', shopController.postCart);
 
-router.get('/checkout', shopControllers.getCheckout);
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
