9ff83b87949ed791a215e7cf9af331c742eafccc
Juan <sagdom96@gmail.com>
2022-07-20 15:01:31 +0200

    update on order button

--- routes/shop.js
+++ routes/shop.js
@@ -18,8 +18,12 @@ router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
+router.get('/create-order', shopController.postOrder);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
+
+
 
 module.exports = router;
