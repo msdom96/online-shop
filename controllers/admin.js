12178ff054f7c7bf3d3d581de1543a536f406c74
Juan <sagdom96@gmail.com>
2022-07-23 14:16:53 +0200

    update

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
-
-const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
 const Product = require('../models/product');
 
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -53,20 +53,26 @@ exports.getEditProduct = (req, res, next) => {
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  const db = getDb();
-  const product = db.collection('products').findByPk(prodId).then(product => {
-    product.title = req.body.title;
-    product.price = req.body.price;
-    product.description = req.body.description;
-  }).then(() => { return product }).save().then(() => {
-    console.log('UPDATED PRODUCT');
-    res.redirect('/admin/products')
-  }).catch(err => {
-    console.log(err)
-  });
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    new ObjectId(prodId)
+  );
+  product
+    .save()
+    .then(result => {
+      console.log('UPDATED PRODUCT!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
-
-
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
--- models/product.js
+++ models/product.js
@@ -1,5 +1,4 @@
 const mongodb = require('mongodb');
-
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -8,18 +7,19 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     let dbOp;
-    if (this.id) {
-      dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this });
-    } else {
+    if (this._id) {
+      // Update the product
       dbOp = db
         .collection('products')
-        .insertOne(this);
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
     }
     return dbOp
       .then(result => {
@@ -29,16 +29,6 @@ class Product {
         console.log(err);
       });
   }
-  static fetchAll() {
-    const db = getDb();
-    return db.collection('products').find().toArray().then(products => {
-      console.log(products);
-      return products;
-    }).catch(err => { console.log(err); });
-  }
-
-
-
 
   static fetchAll() {
     const db = getDb();
@@ -54,9 +44,12 @@ class Product {
         console.log(err);
       });
   }
-  static findByPk(prodId) {
+
+  static findById(prodId) {
     const db = getDb();
-    return db.collection('products').find({ _id: new mongodb.ObjectId(prodId) })
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
       .then(product => {
         console.log(product);
