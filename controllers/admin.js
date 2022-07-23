7adb6152ce9acc7765215c6273f46ed4b5f68d13
Juan <sagdom96@gmail.com>
2022-07-23 14:18:24 +0200

    updaterino

--- controllers/admin.js
+++ controllers/admin.js
@@ -35,7 +35,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
+  Product.findById(prodId)
     // Product.findById(prodId)
     .then(product => {
       if (!product) {
