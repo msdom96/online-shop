164264fb2b81202a7211989520baee0fdf3f467c
Juan <sagdom96@gmail.com>
2022-07-25 14:52:12 +0200

    update

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,4 @@
-const mongodb = require('mongodb');
 const Product = require('../models/product');
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -16,7 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
   product
     .save()
     .then(result => {
@@ -63,7 +67,7 @@ exports.postEditProduct = (req, res, next) => {
     updatedPrice,
     updatedDesc,
     updatedImageUrl,
-    new ObjectId(prodId)
+    prodId
   );
   product
     .save()
--- util/database.js
+++ util/database.js
@@ -4,7 +4,9 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = callback => {
-  MongoClient.connect('mongodb+srv://msdom:Joaquim17@cluster0.l6squme.mongodb.net/?retryWrites=true&w=majority')
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
     .then(client => {
       console.log('Connected!');
       _db = client.db();
@@ -25,11 +27,3 @@ const getDb = () => {
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize('node', 'root', 'Joaquim17', {
-//   dialect: 'mysql',
-//   host: 'localhost'
-// });
-
-// module.exports = sequelize;
