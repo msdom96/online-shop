e02ad53ee939b824c9c02da1993942b2e8b08dca
Juan <sagdom96@gmail.com>
2022-07-25 21:05:16 +0200

    update cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,18 +53,15 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then(products => {
+
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
+
     .catch(err => console.log(err));
 };
 
@@ -76,6 +73,8 @@ exports.postCart = (req, res, next) => {
     })
     .then(result => {
       console.log(result);
+    }).then(() => {
+      res.redirect('/cart');
     });
   // let fetchedCart;
   // let newQuantity = 1;
