e6483c6cc25b8802dc5afc15947f9764399c51f3
Juan <sagdom96@gmail.com>
2022-07-25 18:59:36 +0200

    update maximilian

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://msdom:Joaquim17@cluster0.l6squme.mongodb.net/?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
