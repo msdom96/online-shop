5283b4c7620acd584b7454fa91f7c3ba84d8fbf4
Juan <sagdom96@gmail.com>
2022-07-28 22:57:03 +0200

    cart mongoose

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('62deb2883342956d4c23f934')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('62e2ebcf7d804a06da7beb79')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +34,21 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://msdom:Joaquim17@cluster0.l6squme.mongodb.net/shop?retryWrites=true&w=majority').then(result => {
+
+  User.findOne().then(user => {
+    if (!user) {
+      const user = new User({
+        name: 'Dom',
+        email: 'sagdom96@gmail.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  });
+
+
   app.listen(3000);
 }).catch(err => {
   console.log(err);
