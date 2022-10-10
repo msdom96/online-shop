d0752da72bf6f3a7dd644bf19ea64d2eb2858007
Juan <sagdom96@gmail.com>
2022-10-10 12:17:04 +0200

    orders update

--- models/user.js
+++ models/user.js
@@ -42,6 +42,11 @@ userSchema.methods.removeFromCart = function (productId) {
   });
   this.cart.items = updatedCartItems;
   return this.save();
+};
+
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  return this.save();
 }
 
 
